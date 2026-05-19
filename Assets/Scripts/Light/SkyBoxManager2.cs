a532f45c3332f113a7ad8b1ce79160964199c967
LioOpss <corneliusnathanael@gmail.com>
2026-05-20 01:37:10 +0700

    night day cycle, script background

---
+++ Assets/Scripts/Light/SkyBoxManager2.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxManager2 : MonoBehaviour
+{
+    public LightingManager lightingManager;
+    public Material[] skyboxMaterials = new Material[5];
+
+    private int lastAppliedIndex = -1;
+
+    void Update()
+    {
+
+        if (lightingManager == null || skyboxMaterials.Length < 5) return;
+
+        float currentTime = lightingManager.TimeOfDay;
+
+        float scaledTime = (currentTime / 24f) * 5f;
+        int currentIdx = Mathf.FloorToInt(scaledTime) % 5;
+
+
+        if (currentIdx != lastAppliedIndex)
+        {
+            if (skyboxMaterials[currentIdx] != null)
+            {
+                RenderSettings.skybox = skyboxMaterials[currentIdx];
+
+                DynamicGI.UpdateEnvironment();
+
+                lastAppliedIndex = currentIdx;
+            }
+        }
+    }
+}
