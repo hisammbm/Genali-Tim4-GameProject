0e69c580b023c6c76ca1b3a29cd546da97f74db1
hisammbm <hisammbm@gmail.com>
2026-05-31 17:49:42 +0700

    Membuat Script HighScore

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 public class GameManager : MonoBehaviour
 {
@@ -8,16 +9,30 @@ public class GameManager : MonoBehaviour
 
     [Header("Score")]
     [SerializeField] private TMP_Text txtScore;
+    [SerializeField] private TMP_Text txtHighScore;
     private int currentScore = 0;
+    private int highScore = 0;
 
     private void Awake()
     {
         instance = this;
     }
 
+    void Start()
+    {
+        highScore = PlayerPrefs.GetInt("highScore", 0);
+        txtScore.text = currentScore.ToString();
+        txtHighScore.text = highScore.ToString();
+    }
+
     public void AddScore(int score)
     {
         currentScore += score;
         txtScore.text = $"{currentScore}";
+
+        if (highScore < currentScore)
+        {
+            PlayerPrefs.SetInt("highScore", currentScore);
+        }
     }
 }
